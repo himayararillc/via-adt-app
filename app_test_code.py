7545f69356df5a708f7f49fe09a50cd123c2304e
himayararillc <owishopjp.amazon@gmail.com>
2026-08-18 16:32:20 +0900

    Update app_test_code.py

--- app_test_code.py
+++ app_test_code.py
@@ -6,7 +6,7 @@ import re
 from collections import OrderedDict, defaultdict
 
 import streamlit as st
-from google import genei
+from google import generativeai
 
 # ---------------------------------------------------------
 # 1. ページ基本設定
